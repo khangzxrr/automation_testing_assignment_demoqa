69f303f0e743370ae9ca68812e58b6cbae535c34
khangzxrr <khangzxrr@gmail.com>
2025-05-13 22:56:20 +0700

    add driverFactory

---
+++ Drivers/DriverFactory.cs
@@ -0,0 +1,60 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+public static class DriverFactory
+{
+    public static IWebDriver MakeDriver(DriverType type, dynamic? options = null)
+    {
+        if (type == DriverType.Chrome)
+        {
+            return new ChromeDriver((ChromeOptions?)options ?? GetChromeOptions());
+        }
+
+        if (type == DriverType.Firefox)
+        {
+            return new FirefoxDriver((FirefoxOptions?)options ?? GetFirefoxOptions());
+        }
+
+        if (type == DriverType.Edge)
+        {
+            return new EdgeDriver((EdgeOptions?)options ?? GetEdgeOptions());
+        }
+
+        throw new ArgumentException("Unsupported browser");
+    }
+
+    public static ChromeOptions GetChromeOptions()
+    {
+        var options = new ChromeOptions
+        {
+            PageLoadStrategy = PageLoadStrategy.Eager,
+            PageLoadTimeout = TimeSpan.FromSeconds(60),
+        };
+
+        options.AddUserProfilePreference("profile.managed_default_content_settings.images", 2);
+        options.AddArgument("--headless");
+
+        return options;
+    }
+
+    public static FirefoxOptions GetFirefoxOptions()
+    {
+        var options = new FirefoxOptions
+        {
+            PageLoadStrategy = PageLoadStrategy.Eager,
+            PageLoadTimeout = TimeSpan.FromSeconds(60),
+        };
+
+        options.AddArgument("--headless");
+        options.SetPreference("permissions.default.image", 2);
+
+        return options;
+    }
+
+    public static EdgeOptions GetEdgeOptions()
+    {
+        return new EdgeOptions();
+    }
+}
---
+++ Drivers/DriverType.cs
@@ -0,0 +1,6 @@
+public enum DriverType
+{
+    Chrome,
+    Firefox,
+    Edge,
+}
