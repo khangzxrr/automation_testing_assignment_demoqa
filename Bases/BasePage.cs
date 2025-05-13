b7410a6f7844a296a418c0f71855db1bffb57358
khangzxrr <khangzxrr@gmail.com>
2025-05-13 22:56:08 +0700

    fresh action, custom drag and drop

--- Bases/BasePage.cs
+++ Bases/BasePage.cs
@@ -6,14 +6,13 @@ public abstract class BasePage
 {
     protected IWebDriver driver;
     protected WebDriverWait wait;
-    protected Actions actions;
+    protected Actions actions => new Actions(driver); //should use fresh actions
 
     protected BasePage(IWebDriver driver, int timeoutInSeconds = 15)
     {
         this.driver = driver;
 
         wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-        actions = new Actions(driver);
     }
 
     protected IWebElement WaitUntilVisible(By locator, int timeoutInSeconds = 15)
@@ -46,11 +45,31 @@ public abstract class BasePage
         return elements.Select(e => new WebElement(driver, e, wait, locator)).ToList();
     }
 
-
     public WebElement FindWithoutWaiting(By locator)
     {
         var element = driver.FindElement(locator);
 
         return new WebElement(driver, element, wait, locator);
+    }
+
+    public void DragAndDrop(WebElement source, WebElement target)
+    {
+        actions
+            .ClickAndHold(source.source)
+            .Pause(TimeSpan.FromMilliseconds(500))
+            .MoveToElement(target.source)
+            .Release()
+            .Pause(TimeSpan.FromMilliseconds(500))
+            .Perform();
+    }
+
+    public void WaitForPageReady(int timeoutInSeconds = 60)
+    {
+        new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds)).Until(d =>
+        {
+            var js = (IJavaScriptExecutor)d;
+            return js.ExecuteScript("return document.readyState").ToString() == "complete"
+                && (bool)(js.ExecuteScript("return window.jQuery ? jQuery.active === 0 : true"));
+        });
     }
 }
