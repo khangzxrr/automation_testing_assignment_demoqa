34299c7659021aa0d866af5718bb426985752de3
khangzxrr@gmail.com <khangzxrr@gmail.com>
2025-05-16 10:47:18 +0700

    return filePath

--- Core/Utils/ScreenshotHelper.cs
+++ Core/Utils/ScreenshotHelper.cs
@@ -2,12 +2,18 @@ using OpenQA.Selenium;
 
 public static class ScreenshotHelper
 {
-    public static void Capture(IWebDriver driver, string testname)
+    public static string Capture(IWebDriver driver, string testname)
     {
         var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+
         var dir = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots");
+
         Directory.CreateDirectory(dir);
+
         var file = Path.Combine(dir, $"{testname.Trim()}_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+
         screenshot.SaveAsFile(file);
+
+        return file;
     }
 }
