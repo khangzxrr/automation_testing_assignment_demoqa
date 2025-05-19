a374057b414be3d9896ed3854a1bf209df9875b0
khangzxrr <khangzxrr@gmail.com>
2025-05-19 22:38:33 +0700

    fix path issue in linux/window

--- Core/DataLoaders/DataLoaderStrategies/JsonDataLoader.cs
+++ Core/DataLoaders/DataLoaderStrategies/JsonDataLoader.cs
@@ -2,7 +2,6 @@ using Newtonsoft.Json.Linq;
 
 public class JsonDataLoader : IDataLoaderStrategy
 {
-
     public IEnumerable<object[]> LoadData(string filepath)
     {
         var json = File.ReadAllText(filepath);
@@ -13,6 +12,13 @@ public class JsonDataLoader : IDataLoaderStrategy
             var values = new List<object>();
             foreach (var prop in obj.Properties())
             {
+                var objValue = prop.Value.ToString();
+
+                if (prop.Name == "ImageName")
+                {
+                    objValue = Path.Combine(objValue.Split('/', '\\'));
+                }
+
                 values.Add(prop.Value.ToString()); // or .ToObject<object>() for raw values
             }
 
